1265539657aaf3e845e971a441f5d111f388e920
Vladislav Troinich <vladislav@troinich.pro>
2026-01-19 00:47:59 +0300

    Revert back prev version of ibapi. So revert error handler

--- atreyu_backtrader_api/ibstore.py
+++ atreyu_backtrader_api/ibstore.py
@@ -458,7 +458,7 @@ class IBApi(EWrapper, EClient):
         self.cb.commissionReport(commissionReport)
 
     @logibmsg
-    def error(self, reqId, errorTime, errorCode, errorString, advancedOrderRejectJson = ""):
+    def error(self, reqId, errorCode, errorString, advancedOrderRejectJson = ""):
         self.cb.error(ErrorMsg(reqId, errorCode, errorString, advancedOrderRejectJson))
 
     @logibmsg
